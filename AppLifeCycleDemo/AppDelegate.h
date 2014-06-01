2e3f82d9ca3cca1f70d3cbf3553230f7132c34e2
liuchangjiang <liuchangjiang@liuchangjiangdeMac-mini.local>
2014-06-01 10:22:18 +0800

    Initial Commit

---
+++ AppLifeCycleDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppLifeCycleDemo
+//
+//  Created by liuchangjiang on 14-6-1.
+//  Copyright (c) 2014年 demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppLifeCycleDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AppLifeCycleDemo
+//
+//  Created by liuchangjiang on 14-6-1.
+//  Copyright (c) 2014年 demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
